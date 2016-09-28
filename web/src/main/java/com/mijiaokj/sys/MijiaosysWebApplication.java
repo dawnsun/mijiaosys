2fcc28cb4b7c59fad8cd8e5d1c1410c591744fae
scg16@126.com <scg16@126.com>
2016-09-29 02:12:11 +0800

    add file

---
+++ web/src/main/java/com/mijiaokj/sys/MijiaosysWebApplication.java
@@ -0,0 +1,12 @@
+package com.mijiaokj.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijiaosysWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijiaosysWebApplication.class, args);
+	}
+}
