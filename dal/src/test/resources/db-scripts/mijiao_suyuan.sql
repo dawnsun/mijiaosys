cf6bf5168ee5c1acc6f76b485e416537a141695f
scg16@126.com <scg16@126.com>
2017-06-15 00:24:14 +0800

    土地作物管理表结构设计

--- dal/src/test/resources/db-scripts/mijiao_suyuan.sql
+++ dal/src/test/resources/db-scripts/mijiao_suyuan.sql
@@ -21,6 +21,46 @@ create table LAND_RES
 alter table LAND_RES comment '土地资源表';
 
 /*==============================================================*/
+/* Table: CROPS                                              */
+/*==============================================================*/
+create table CROPS
+(
+   id                   bigint(20) unsigned not null auto_increment,
+   crops_name            varchar(64) not null comment '农作物名称',
+   menu_icon            varchar(32) not null comment '农作物图标',
+   parent_id            bigint(20) unsigned not null comment '父农作物ID',
+   crops_order           int(10) not null comment '农作物排序',
+   gmt_create           datetime not null comment '创建时间',
+   gmt_modified         datetime not null comment '修改时间',
+   creator              varchar(32) not null comment '创建人',
+   modifier             varchar(32) not null comment '修改人',
+   is_delete            tinyint(1) not null  DEFAULT '0' comment '0-有效 1-删除',
+   primary key (id)
+);
+
+alter table CROPS comment '农作物表';
+
+/*==============================================================*/
+/* Table: TESTING_INSTITUTION                                              */
+/*==============================================================*/
+create table TESTING_INSTITUTION
+(
+   id                   bigint(20) unsigned not null auto_increment,
+   institution_name            varchar(64) not null comment '检测机构名称',
+   institution_address         varchar(32) not null comment '检测机构地址',
+   institution_contacts        varchar(32) not null comment '检测机构联系人',
+   institution_phone           varchar(16) not null comment '检测机构电话',
+   gmt_create           datetime not null comment '创建时间',
+   gmt_modified         datetime not null comment '修改时间',
+   creator              varchar(32) not null comment '创建人',
+   modifier             varchar(32) not null comment '修改人',
+   is_delete            tinyint(1) not null  DEFAULT '0' comment '0-有效 1-删除',
+   primary key (id)
+);
+
+alter table TESTING_INSTITUTION comment '检测机构表';
+
+/*==============================================================*/
 /* Table: LAND_QUALITY                                          */
 /*==============================================================*/
 create table LAND_QUALITY
@@ -29,11 +69,11 @@ create table LAND_QUALITY
    land_id              bigint(20) unsigned not null comment '土地资源主键',
    over_elements        varchar(16)  comment '超标元素',
    missing_elements     varchar(16)  comment '缺失元素',
-   alias                varchar(32)  comment '酸碱度',
-   address              varchar(128)  comment '检测机构',
-   gmt_create           datetime not null comment '监测时间',
-   size                 varchar(16)  comment '检测附件',
-   size                 varchar(16)  comment '当前土地照片附件id多张用逗号隔开',
+   ph                varchar(2)  comment '酸碱度',
+   testing_id              varchar(128)  comment '检测机构id',
+   gmt_testing           datetime not null comment '监测时间',
+   testing_file                 varchar(16)  comment '检测附件',
+   land_img                 varchar(16)  comment '当前土地照片附件id多张用逗号隔开',
    gmt_create           datetime not null comment '创建时间',
    gmt_modified         datetime not null comment '修改时间',
    creator              varchar(32) not null comment '创建人',
@@ -43,3 +83,15 @@ create table LAND_QUALITY
 );
 
 alter table LAND_QUALITY comment '土地质量监测表';
+
+
+季度土地作物表
+季度年份 季度份 土地id 管理者id 作物id 作物数量
+
+日常作物操作表
+
+作物操作配置表
+
+作物产值表
+
+作物规格表
